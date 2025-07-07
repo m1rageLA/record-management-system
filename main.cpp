3354aaa4c144963925ec62c382cf324f4ec0f38d
m1rage <miragela.code@gmail.com>
2025-07-07 21:10:47 +0200

    Implement basic menu functionality in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello, C++ in VsCode" << std::endl;
+    int choice; // объявляем вне цикла
+
+    do
+    {
+        std::string text =
+            "1. Add record\n"
+            "2. Search record\n"
+            "3. Update record\n"
+            "4. Delete record\n"
+            "5. Exit\n"
+            "Enter your choice: ";
+
+        std::cout << text;
+        std::cin >> choice;
+
+        // можно сюда добавить обработку выбора
+        switch (choice) {
+            case 1:
+                std::cout << "Adding record...\n";
+                break;
+            case 2:
+                std::cout << "Searching...\n";
+                break;
+            case 3:
+                std::cout << "Updating...\n";
+                break;
+            case 4:
+                std::cout << "Deleting...\n";
+                break;
+            case 5:
+                std::cout << "Exiting...\n";
+                break;
+            default:
+                std::cout << "Invalid choice.\n";
+        }
+
+    } while (choice != 5); // пока не выбрал выход
+
     return 0;
 }
